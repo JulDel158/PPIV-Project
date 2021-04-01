35621c197bd10176341ff1a6b4c4c751e571f8da
Angel Rivera Velez <angel.riveravelez3@gmail.com>
2021-04-01 16:52:39 -0400

    Shaders moved

--- GreenScreen/renderer.h
+++ GreenScreen/renderer.h
@@ -3,40 +3,7 @@
 #include <d3dcompiler.h>
 #include "test_pyramid.h"
 #pragma comment(lib, "d3dcompiler.lib")
-// Simple Vertex Shader
-const char* vertexShaderSource = R"(
-// by default HLSL is COLUMN major
-// 
-//constant buffer
-cbuffer SHADER_VARS
-{
-	matrix w;
-	matrix v;
-	matrix p;
-}
 
-// an ultra simple hlsl vertex shader
-float4 main(float3 local_pos : POSITION,
-			float3 local_uvw : UVW,
-			float3 local_nrm : NRM) : SV_POSITION
-{
-	float4 proj_pos = float4(local_pos, 1);
-	// do w * v * p
-	proj_pos = mul(w, proj_pos);
-	proj_pos = mul(v, proj_pos);
-	proj_pos = mul(p, proj_pos);
-
-	return proj_pos;
-}
-)";
-// Simple Pixel Shader
-const char* pixelShaderSource = R"(
-// an ultra simple hlsl pixel shader
-float4 main() : SV_TARGET 
-{	
-	return float4(0.25f,0.0f,1.0f,0); 
-}
-)";
 // Creation, Rendering & Cleanup
 class Renderer
 {
