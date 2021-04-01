877c4315e9040a692855935232a74895a1c9ba30
Julio Delgado Abreu <JJDelgadoAbreu@student.fullsail.edu>
2021-04-01 13:29:22 -0400

    Add test_pyramid.h Changes in renderer.h to draw in 3d and indexed

--- GreenScreen/main.cpp
+++ GreenScreen/main.cpp
@@ -2,6 +2,7 @@
 #define GATEWARE_ENABLE_CORE // All libraries need this
 #define GATEWARE_ENABLE_SYSTEM // Graphics libs require system level libraries
 #define GATEWARE_ENABLE_GRAPHICS // Enables all Graphics Libraries
+#define GATEWARE_ENABLE_MATH	//math library 
 // Ignore some GRAPHICS libraries we aren't going to use
 #define GATEWARE_DISABLE_GDIRECTX12SURFACE // we have another template for this
 #define GATEWARE_DISABLE_GRASTERSURFACE // we have another template for this
@@ -10,6 +11,7 @@
 // With what we want & what we don't defined we can include the API
 #include "../Gateware/Gateware.h"
 #include "renderer.h" // example rendering code (not Gateware code!)
+
 // open some namespaces to compact the code a bit
 using namespace GW;
 using namespace CORE;
--- GreenScreen/renderer.h
+++ GreenScreen/renderer.h
@@ -1,13 +1,32 @@
 // minimalistic code to draw a single triangle, this is not part of the API.
 // required for compiling shaders on the fly, consider pre-compiling instead
 #include <d3dcompiler.h>
+#include "test_pyramid.h"
 #pragma comment(lib, "d3dcompiler.lib")
 // Simple Vertex Shader
 const char* vertexShaderSource = R"(
-// an ultra simple hlsl vertex shader
-float4 main(float2 inputVertex : POSITION) : SV_POSITION
+// by default HLSL is COLUMN major
+// 
+//constant buffer
+cbuffer SHADER_VARS
 {
-	return float4(inputVertex, 0, 1);
+	matrix w;
+	matrix v;
+	matrix p;
+}
+
+// an ultra simple hlsl vertex shader
+float4 main(float3 local_pos : POSITION,
+			float3 local_uvw : UVW,
+			float3 local_nrm : NRM) : SV_POSITION
+{
+	float4 proj_pos = float4(local_pos, 1);
+	// do w * v * p
+	proj_pos = mul(w, proj_pos);
+	proj_pos = mul(v, proj_pos);
+	proj_pos = mul(p, proj_pos);
+
+	return proj_pos;
 }
 )";
 // Simple Pixel Shader
@@ -26,9 +45,23 @@ class Renderer
 	GW::GRAPHICS::GDirectX11Surface d3d;
 	// what we need at a minimum to draw a triangle
 	Microsoft::WRL::ComPtr<ID3D11Buffer>		vertexBuffer;
+	Microsoft::WRL::ComPtr<ID3D11Buffer>		indexBuffer;
 	Microsoft::WRL::ComPtr<ID3D11VertexShader>	vertexShader;
 	Microsoft::WRL::ComPtr<ID3D11PixelShader>	pixelShader;
 	Microsoft::WRL::ComPtr<ID3D11InputLayout>	vertexFormat;
+	// world view proj
+	Microsoft::WRL::ComPtr<ID3D11Buffer>		constantBuffer;
+	struct SHADER_VARS
+	{
+		GW::MATH::GMATRIXF w = GW::MATH::GIdentityMatrixF;
+		GW::MATH::GMATRIXF v = GW::MATH::GIdentityMatrixF;
+		GW::MATH::GMATRIXF p = GW::MATH::GIdentityMatrixF;
+
+	}Vars;
+	// math library handle
+	GW::MATH::GMatrix m;
+
+
 public:
 	Renderer(GW::SYSTEM::GWindow _win, GW::GRAPHICS::GDirectX11Surface _d3d)
 	{
@@ -37,14 +70,19 @@ public:
 		ID3D11Device* creator;
 		d3d.GetDevice((void**)&creator);
 		// Create Vertex Buffer
-		float verts[] = {
+		/*float verts[] = {
 			   0,   0.5f,
 			 0.5f, -0.5f,
 			-0.5f, -0.5f
-		};
-		D3D11_SUBRESOURCE_DATA bData = { verts, 0, 0 };
-		CD3D11_BUFFER_DESC bDesc(sizeof(verts), D3D11_BIND_VERTEX_BUFFER);
+		};*/
+		D3D11_SUBRESOURCE_DATA bData = { test_pyramid_data, 0, 0 };
+		CD3D11_BUFFER_DESC bDesc(sizeof(test_pyramid_data), D3D11_BIND_VERTEX_BUFFER);
 		creator->CreateBuffer(&bDesc, &bData, vertexBuffer.GetAddressOf());
+		// create index
+		D3D11_SUBRESOURCE_DATA iData = { test_pyramid_indicies, 0, 0 };
+		CD3D11_BUFFER_DESC iDesc(sizeof(test_pyramid_indicies), D3D11_BIND_INDEX_BUFFER);
+		creator->CreateBuffer(&iDesc, &iData, indexBuffer.GetAddressOf());
+		
 		// Create Vertex Shader
 		UINT compilerFlags = D3DCOMPILE_ENABLE_STRICTNESS;
 #if _DEBUG
@@ -74,13 +112,37 @@ public:
 		// Create Input Layout
 		D3D11_INPUT_ELEMENT_DESC format[] = {
 			{ 
-				"POSITION", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 
+				"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 
 				D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 
+			},
+			{
+				"UVW", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
+				D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0
+			},
+			{
+				"NRM", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
+				D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0
 			}
 		};
 		creator->CreateInputLayout(format, ARRAYSIZE(format), 
 			vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), 
 			vertexFormat.GetAddressOf());
+
+		//init math stuff
+		m.Create();
+		m.LookAtLHF(GW::MATH::GVECTORF{ 0.5f, 1.0f, -2.0f }, //eye
+					GW::MATH::GVECTORF{ 0,0.5f,0 }, //at
+					GW::MATH::GVECTORF{ 0,1,0 }, //up
+					Vars.v);
+		float ar;
+		d3d.GetAspectRatio(ar);
+		m.ProjectionDirectXLHF(G_PI_F / 2.0f, ar, 0.01f, 100, Vars.p);
+
+		// create constant buffer
+		D3D11_SUBRESOURCE_DATA cData = { &Vars, 0, 0 };
+		CD3D11_BUFFER_DESC cDesc(sizeof(Vars), D3D11_BIND_CONSTANT_BUFFER);
+		creator->CreateBuffer(&cDesc, &cData, constantBuffer.GetAddressOf());
+
 		// free temporary handle
 		creator->Release();
 	}
@@ -94,16 +156,22 @@ public:
 		// setup the pipeline
 		ID3D11RenderTargetView *const views[] = { view };
 		con->OMSetRenderTargets(ARRAYSIZE(views), views, nullptr);
-		const UINT strides[] = { sizeof(float) * 2 };
+		const UINT strides[] = { sizeof(OBJ_VERT) };
 		const UINT offsets[] = { 0 };
 		ID3D11Buffer* const buffs[] = { vertexBuffer.Get() };
 		con->IASetVertexBuffers(0, ARRAYSIZE(buffs), buffs, strides, offsets);
+		con->IASetIndexBuffer(indexBuffer.Get(), DXGI_FORMAT_R32_UINT, 0);
 		con->VSSetShader(vertexShader.Get(), nullptr, 0);
 		con->PSSetShader(pixelShader.Get(), nullptr, 0);
 		con->IASetInputLayout(vertexFormat.Get());
+		// send in the constant buffer
+		ID3D11Buffer* const constants[] = { constantBuffer.Get() };
+		con->VSSetConstantBuffers(0, 1, constants);
+		// to update dynamically *****
+		// con->UpdateSubresource()
 		// now we can draw
 		con->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-		con->Draw(3, 0);
+		con->DrawIndexed(test_pyramid_indexcount, 0, 0);
 		// release temp handles
 		view->Release();
 		con->Release();
